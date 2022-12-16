ac0c308523df8b66ceaea0e6efcf7503ed3285b1
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-16 17:59:39 +0700

    initial not working

--- M_CEA_2/M_CEA_2.f90
+++ M_CEA_2/M_CEA_2.f90
@@ -15,13 +15,49 @@
     program M_CEA_2
 
     implicit none
+    include 'Cea.inc'
 
     ! Variables
+    character*15 ensert(20)
+    character*200 infile, ofile
+    character*196 prefix
+    logical caseok, ex, readok
+    integer i, inc, iof, j, ln, n
+    integer INDEX
+    real*8 xi, xln
+    real*8 DLOG
+    
+    Save caseok, ensert, ex, i, inc, infile, Pfile, iof, j, ln, n, ofile, prefix, readok, xi, xln
+    
+    Write(*, 99001)
+    Read(*, 99002)prefix
+    ln = INDEX(prefix, ' ')-1
+    infile = prefix(1:ln)//'.inp'
+    ofile = prefix(1:ln)//'.out'
+    Pfile = prefix(1:ln)//'.plt'
+    inquire(File = infile, Exist=ex)
+    if(.not.ex)then
+        print *, infile, 'file not found'
+        Goto 400
+    end if
+    
+    if(ex)then
+        print *, infile, 'file found'
+    end if
+    
+    readok = .true.
+    Newr = .false.
+    
 
     ! Body of M_CEA_2
-    print *, 'Hello World'
+    
     
     Read(*,*)
+    
+400 stop    
+    
+99001 FORMAT (//'Enter file name'//)
+99002 FORMAT (1X, A16, 'Inserted')      
 
     end program M_CEA_2
 
